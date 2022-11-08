2522b7b66b6ee4573873971f69f00e9fe1720cf0
Sslim123 <slimsmasm5@gmail.com>
2022-11-08 15:56:19 +0000

    laptop_request form
    submit the form and disappear the pop_up message appear with text thank

--- client/src/pages/Home/Forms/LaptopRequest/LaptopForm.js
+++ client/src/pages/Home/Forms/LaptopRequest/LaptopForm.js
@@ -1,3 +1,4 @@
+import { doc } from "prettier";
 import React, { useState } from "react";
 import "./LaptopForm.css";
 
@@ -6,7 +7,6 @@ function LaptopForm() {
 	const [lastName, setLastName] = useState("");
 	const [email, setEmail] = useState("");
 	const [phoneNumber, setNumber] = useState("");
-
 	function handleClick(e) {
 		if (e.target.name === "firstName") {
 			setFirst(e.target.value);
@@ -37,58 +37,91 @@ function LaptopForm() {
 			}),
 			headers: { "content-type": "application/json" },
 		});
-	}
-	function messageAlert() {
-		alert(
-			"thank you for your completing the request form. your request have been recived "
-		);
-	}
-	return (
-		<div className="form-card">
+	};
+	const closeModale = document.querySelector('.close_modale');
+			const  btn = document.querySelector('.btn1');
+		const modale = document.querySelector('.modale-content');
+		const form = document.querySelector('.form');
+
+	//function display greeting after submit the from
+		function messageGreet(){
+		
+			btn.addEventListener("click", messageAlert);
+			function messageAlert() {
+				console.log('heloo');
+				modale.style.display = 'block';
+		};
+		closeModale.addEventListener('click', messageClosed);
+		function messageClosed() {
+			console.log('heloo');
+			modale.style.display = 'none';
+		};
+		btn.addEventListener('click', formApear);
+		function formApear() {
+			console.log('heloo');
+			form.style.display = 'none';			
+				};		
+	};
+
+		return (
+			<div className="form-card">
+			<div className="modale-content">
+              <span className="close_modale">
+               &times;
+			  </span>
+			  <p className="newPragraph">
+							thank you for your completing the request 
+							form. your request have been recived and you 
+							have been added to our waiting list</p>
+			</div>
 			<form onSubmit={submitForm} className="form">
 			<div className="form-conatiner">
-			<label>First Name</label>
+			<label className="class_label">First Name </label>
 				<input 
-				    required
+				
 					type="text"
 					value={firstName}
 					name="firstName"
 					placeholder="first name"
 					className="firstInput"
 					onChange={handleClick}
+					required
 				/>
-				<label>Last Name</label>
+				<label className="class_label">Last  Name</label>
 				<input 
-				    required
+				    
 					type="text"
 					value={lastName}
 					name="lastName"
 					placeholder="lastName"
 					className="firstInput"
 					onChange={handleClick}
+					required
 				/>
-				<label>Email address</label>
+				<label className="class_label">Email address</label>
 				<input
-				    required
+				    
 					type="text"
 					value={email}
 					name="email"
 					placeholder="email"
 					className="firstInput"
 					onChange={handleClick}
+					required
 				/>
-				<label>Phone Number</label>
+				<label className="class_label">phon number </label>
+
 				<input
-				    required
+				    
 					type="number"
 					value={phoneNumber}
 					name="phoneNumber"
 					placeholder="phon number"
 					className="firstInput"
 					onChange={handleClick}
+					required
 				/>
-				<button type="submit"
-				onClick={messageAlert}
+				<button type="submit" onClick={messageGreet}		
 				className="btn1">
 					Submit
 				</button>
