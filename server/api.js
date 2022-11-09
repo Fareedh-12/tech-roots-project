8f808aa01bc797c69648bbb2cf20164723709ab8
Sslim123 <slimsmasm5@gmail.com>
2022-11-09 08:46:43 +0000

    laptop_request

--- server/api.js
+++ server/api.js
@@ -10,17 +10,13 @@ router.get("/", (_, res) => {
 	res.json({ message: "Hello, world!" });
 });
 router.post("/laptop_request", (req, res) => {
-	let firstName = req.body.firstname;
-	let lastName = req.body.lastname;
-	let email = req.body.email;
-	let phoneNumber = req.body.phonenumber;
 	let firstName = req.body.firstName;
 	let lastName = req.body.lastName;
 	let email = req.body.email;
 	let phoneNumber = req.body.phoneNumber;
+
 	const query =
-		" insert into laptop_request (First_Name, Last_Name, Email_Address, phone_Number) values ($1, $2, $3, $4)";
-		" insert into laptop_request (firstname, lastname, email, phonenumber) values ($1, $2, $3, $4)";
+		" insert into laptop_request (firstName, lastName, email, phoneNumber) values ($1, $2, $3, $4)";
 	db.query(query, [firstName, lastName, email, phoneNumber])
 		.then(() => res.send("result.rows"))
 		.catch((error) => {
@@ -35,11 +31,10 @@ router.get("/laptop_request", async (req, res) => {
 
 		const laptopRequests = result.rows.map((row) => {
 			return {
-
-				firstName: row.firstname,
-				lastName: row.lastname,
+				firstName: row.firstName,
+				lastName: row.lastName,
 				email: row.email,
-				phoneNumber: row.phonenumber,
+				phoneNumber: row.phoneNumber,
 			};
 		});
 		res.json(laptopRequests);
